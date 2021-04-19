56b54c90b63b070326858f3d9793dbaf506a4ad8
Ilya Chervyakov <Chervyakov3498@yandex.ru>
2021-04-19 20:23:22 +0700

    lesson12

---
+++ lesson12/script/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed'),
+    todoRemove = document.querySelector('.todo-remove');
+
+let todoData = [];
+
+
+
+const render = function() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        
+            li.innerHTML = '<span class="text-todo">' + item.value +'</span>' + 
+                        '<div class="todo-buttons">' + 
+                            '<button class="todo-remove"></button>' + 
+                            '<button class="todo-complete"></button>' + 
+                        '</div>';
+        if(item.completed){
+            todoCompleted.append(li);
+        }else{
+            todoList.append(li);
+        }
+
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+        btnTodoCompleted.addEventListener('click', function(){
+            item.completed = !item.completed;
+            localStorage.setItem('toDo', JSON.stringify(todoData));
+            render();
+        });
+
+        const removeBtn = li.querySelector('.todo-remove');
+        removeBtn.addEventListener('click', function() {
+            const elem = todoData.indexOf(item);
+            if (elem > -1) {
+                todoData.splice(elem, 1);
+                localStorage.setItem('toDo', JSON.stringify(todoData));
+            }
+            render();
+        });
+    });
+};
+
+if (localStorage.getItem('toDo')) {
+    todoData = JSON.parse(localStorage.getItem('toDo'));
+    render();
+}
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false        
+    };
+
+    if(newTodo.value !== ''){
+        todoData.push(newTodo);
+        
+    }
+
+    if(headerInput.value !== ''){
+    headerInput.value= "";
+    }
+
+    render();
+    
+    
+
+    
+});
