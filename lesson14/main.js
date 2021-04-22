41ccefa2c26714dbedf57b37585fb5d680d80aec
Ilya Chervyakov <Chervyakov3498@yandex.ru>
2021-04-22 19:01:07 +0700

    change

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
 const DomElement = function(selector, height, width, bg = '#000', fontSize = '12px') {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
+    this.selector = prompt('Введите block или best');
+    this.height = +prompt('Введите высоту');
+    this.width = +prompt('Введите ширину');
+    this.bg = prompt('Введите цвет на английском');
+    this.fontSize = +prompt('Введите размер шрифта');
 };
 
 DomElement.prototype.createElement = function() {
 
-    let selector = this.selector.split('')[0];
+    // let selector = this.selector.split('')[0];
 
     let div = document.createElement('div');
 
-    if (selector === '.') {
+    if (this.selector === 'block') {
         document.body.appendChild(div);
         div.setAttribute('class', this.selector);
-    } else if ((selector === '#')) {
+    } else if ((this.selector === 'best')) {
         document.body.appendChild(div);
         div.setAttribute('id', this.selector);
     }
@@ -28,9 +28,9 @@ DomElement.prototype.createElement = function() {
                         height: ${this.height}px; 
                         width: ${this.width}px; 
                         background: ${this.bg}; 
-                        fontSize: ${this.fontSize}px; 
+                        font-size: ${this.fontSize}px; 
                         margin-left: 30px`;
 };
 
-const elem = new DomElement('#square', '200', '200', 'red', 14);
+const elem = new DomElement();
 elem.createElement();
