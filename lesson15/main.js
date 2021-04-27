f82398d11d510ed23e07ef6803e1b5af65ab2804
Ilya Chervyakov <Chervyakov3498@yandex.ru>
2021-04-27 17:18:00 +0700

    lessson 15

---
+++ lesson15/main.js
@@ -0,0 +1,17 @@
+class First {
+
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+    
+}
+class Seconds extends First {
+
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+
+}
+const sec= new Seconds();
+sec.hello();
